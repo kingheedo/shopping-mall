6187e64d6b53aa51f0726197de4b3841d875c563
heedo-wang <dhkdgmleh@gmail.com>
2021-01-20 15:17:01 +0900

    이미지 파일을 서버로 보내기

---
+++ server/routes/product.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+//=================================
+//             Product
+//=================================
+
+router.post('/image',(req,res) => {
+
+    
+    //가져온 이미지를 저장을 해주면 된다.
+
+
+})
+
+
+module.exports = router;
