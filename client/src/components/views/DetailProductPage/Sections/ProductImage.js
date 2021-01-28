783c2754c6ad1e1839c7f7d55ebbd070354579c5
heedo-wang <dhkdgmleh@gmail.com>
2021-01-28 15:02:48 +0900

    Product Image 컴포넌트 만들기

---
+++ client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import ImageGallery from 'react-image-gallery';
+function ProductImage(props) {
+    const [Images, setImages] = useState([])
+    useEffect(() => {
+        if(props.details.images && props.details.images.length > 0){
+            let images = []
+            props.details.images.map(item =>{
+                images.push({
+                    original: `http://localhost:5000/${item}`,
+                    thumbnail: `http://localhost:5000/${item}`
+                })
+            })
+            setImages(images)
+        }
+    }, [props.details])
+    const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+    return (
+        <div>
+            <ImageGallery items={Images}/>
+        </div>
+    )
+}
+
+export default ProductImage
