99d894c49cd3110e825bdb7b680543a1f3d579be
heedo-wang <dhkdgmleh@gmail.com>
2021-01-30 15:59:56 +0900

    카트에 들어있는 상품들 가격 계산

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useDispatch} from 'react-redux'
 import {getCartItems} from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock'
 function CartPage(props) {
+    const [Total, setTotal] = useState(0)
     const dispatch = useDispatch()
     useEffect(() => {
 
@@ -16,15 +17,31 @@ function CartPage(props) {
                 })
 
                 dispatch(getCartItems(cartItems,props.user.userData.cart))
+                .then(response => {calculateTotal(response.payload)})
             }
         }
 
     }, [props.user.userData])
 
+    let calculateTotal = (cartDetail) => {
+        let total = 0 ;
+
+        cartDetail.map(item => {
+            total += parseInt(item.price,10) * item.quantity
+        })
+        setTotal(total)
+    }
+
+
     return (
         <div style={{width:'85%', margin: '3rem auto'}}>
+            <h1>My Cart</h1>
             <UserCardBlock products={props.user.cartDetail}/>
+        <div style={{marginTop: '3rem'}}>
+            <h2>Total Amount: ${Total}</h2>
         </div>
+        </div>
+
     )
 }
 
